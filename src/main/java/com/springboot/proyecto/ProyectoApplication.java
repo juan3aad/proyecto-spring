11dd2429528f1e6d1a3660af1603059535a9bae2
juan3aad <juan3aad@gmail.com>
2022-02-26 21:16:47 -0500

    mi primer commit

---
+++ src/main/java/com/springboot/proyecto/ProyectoApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Probando");
+		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
